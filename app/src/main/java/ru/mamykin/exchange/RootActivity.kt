45df51d661addb83aa378f0ece90aa7c0ba2e7a5
Mamykin Andrey <mamykin.av@bk.ru>
2018-08-17 13:25:23 +0500

    Initial commit

---
+++ app/src/main/java/ru/mamykin/exchange/RootActivity.kt
@@ -0,0 +1,12 @@
+package ru.mamykin.exchange
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RootActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
