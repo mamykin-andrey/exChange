1016e5b23dc47dc0bcaf2b1fe06222afbf4aa1ff
Mamykin Andrey <mamykin.av@bk.ru>
2018-08-19 16:32:08 +0500

    Add class for bind AndroidX Fragment and Moxy MVP Delegate

--- app/src/main/java/ru/mamykin/exchange/core/platform/BaseFragment.kt
+++ app/src/main/java/ru/mamykin/exchange/core/platform/BaseFragment.kt
@@ -4,25 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.IdRes
-import androidx.fragment.app.Fragment
 
-abstract class BaseFragment : Fragment() {
+abstract class BaseFragment : AndroidxMvpFragment() {
 
     abstract val layoutId: Int
+
+    open fun onFinish() {
+
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(layoutId, container, false)
     }
 
-    fun replaceFragment(@IdRes containerId: Int, fragment: Fragment, addToBackStack: Boolean = true) {
-        fragmentManager?.let {
-            val transaction = it.beginTransaction()
-            transaction.replace(containerId, fragment)
-            if (addToBackStack)
-                transaction.addToBackStack(null)
-            transaction.commitAllowingStateLoss()
+    override fun onDestroy() {
+        super.onDestroy()
+        if (activity?.isFinishing == true) {
+            onFinish()
         }
     }
 }
