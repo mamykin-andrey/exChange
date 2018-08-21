040679cfef413f6c15e80f6814ae5a1fd6085148
Mamykin Andrey <mamykin.av@bk.ru>
2018-08-21 10:51:27 +0500

    Display currency name on list item subtitle

--- app/src/main/java/ru/mamykin/exchange/ui/viewholder/CurrencyRateViewHolder.kt
+++ app/src/main/java/ru/mamykin/exchange/ui/viewholder/CurrencyRateViewHolder.kt
@@ -7,6 +7,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.item_currency_rate.view.*
 import ru.mamykin.exchange.BuildConfig
+import ru.mamykin.exchange.R
 import ru.mamykin.exchange.core.extension.setOnGetFocusListener
 import ru.mamykin.exchange.core.extension.textChangedEvents
 import ru.mamykin.exchange.core.extension.toFloat
@@ -19,6 +20,7 @@ class CurrencyRateViewHolder(private val context: Context,
 
     companion object {
         private const val DRAWABLE_TYPE = "drawable"
+        private const val STRING_TYPE = "string"
     }
 
     fun bind(rate: Rate, currencyOrAmountChangedFunc: (String, Float) -> Unit) {
@@ -28,12 +30,16 @@ class CurrencyRateViewHolder(private val context: Context,
         bindCurrencyIcon(rate.code)
     }
 
-    private fun bindCurrencySubtitle(rate: Rate) {
-        itemView.currencyNameTextView!!.text = rate.code
-    }
-
     private fun bindCurrencyTitle(rate: Rate) {
         itemView.currencyCodeTextView!!.text = rate.code
+    }
+
+    private fun bindCurrencySubtitle(rate: Rate) {
+        val currencyId = "currency_name_${rate.code.toLowerCase()}"
+        val stringResId = context.resources.getIdentifier(
+                currencyId, STRING_TYPE, BuildConfig.APPLICATION_ID)
+        itemView.currencyNameTextView.setText(
+                if (stringResId != 0) stringResId else R.string.currency_name_unknown)
     }
 
     private fun bindAmountInput(rate: Rate, currencyOrAmountChangedFunc: (String, Float) -> Unit) {
@@ -53,9 +59,10 @@ class CurrencyRateViewHolder(private val context: Context,
     }
 
     private fun bindCurrencyIcon(currencyCode: String) {
+        val currencyId = "cur_icon_${currencyCode.toLowerCase()}"
         val iconResId = context.resources.getIdentifier(
-                "cur_icon_${currencyCode.toLowerCase()}",
-                DRAWABLE_TYPE, BuildConfig.APPLICATION_ID)
-        itemView.currencyIconImageView.setImageResource(iconResId)
+                currencyId, DRAWABLE_TYPE, BuildConfig.APPLICATION_ID)
+        itemView.currencyIconImageView.setImageResource(
+                if (iconResId != 0) iconResId else R.drawable.cur_icon_unknown)
     }
 }
