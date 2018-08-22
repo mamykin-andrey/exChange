ae04c75d638582ecd72c6b4de6dc7560ca9324f2
Mamykin Andrey <mamykin.av@bk.ru>
2018-08-22 14:26:47 +0500

    Fix image blinking on update

---
+++ app/src/main/java/ru/mamykin/exchange/core/platform/UiUtils.kt
@@ -0,0 +1,36 @@
+package ru.mamykin.exchange.core.platform
+
+import android.content.Context
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import ru.mamykin.exchange.BuildConfig
+
+object UiUtils {
+
+    private const val DRAWABLE_TYPE = "drawable"
+    private const val STRING_TYPE = "string"
+
+    @DrawableRes
+    fun getDrawableResId(context: Context,
+                         prefix: String,
+                         id: String,
+                         @DrawableRes defaultDrawableId: Int
+    ): Int {
+        val drawableResName = prefix + id
+        val iconResId = context.resources.getIdentifier(
+                drawableResName, DRAWABLE_TYPE, BuildConfig.APPLICATION_ID)
+        return if (iconResId != 0) iconResId else defaultDrawableId
+    }
+
+    @StringRes
+    fun getStringResId(context: Context,
+                       prefix: String,
+                       id: String,
+                       @StringRes defaultStringId: Int
+    ): Int {
+        val iconResName = prefix + id
+        val stringResId = context.resources.getIdentifier(
+                iconResName, STRING_TYPE, BuildConfig.APPLICATION_ID)
+        return if (stringResId != 0) stringResId else defaultStringId
+    }
+}
--- app/src/main/java/ru/mamykin/exchange/ui/viewholder/CurrencyRateViewHolder.kt
+++ app/src/main/java/ru/mamykin/exchange/ui/viewholder/CurrencyRateViewHolder.kt
@@ -3,43 +3,49 @@ package ru.mamykin.exchange.ui.viewholder
 import android.content.Context
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.item_currency_rate.view.*
-import ru.mamykin.exchange.BuildConfig
 import ru.mamykin.exchange.R
 import ru.mamykin.exchange.core.extension.setOnGetFocusListener
 import ru.mamykin.exchange.core.extension.textChangedEvents
 import ru.mamykin.exchange.core.extension.toFloat
+import ru.mamykin.exchange.core.platform.UiUtils
 import ru.mamykin.exchange.domain.entity.Rate
 import java.util.concurrent.TimeUnit
 
-class CurrencyRateViewHolder(private val context: Context,
-                             itemView: View
+class CurrencyRateViewHolder(
+        private val context: Context,
+        itemView: View
 ) : RecyclerView.ViewHolder(itemView) {
 
     companion object {
-        private const val DRAWABLE_TYPE = "drawable"
-        private const val STRING_TYPE = "string"
+        private const val ICON_PREFIX = "cur_icon_"
+        private const val NAME_PREFIX = "currency_name_"
+        private const val DEFAULT_ICON = R.drawable.cur_icon_unknown
+        private const val DEFAULT_NAME = R.string.currency_name_unknown
     }
 
     fun bind(rate: Rate, currencyOrAmountChangedFunc: (String, Float) -> Unit) {
-        bindCurrencyTitle(rate)
-        bindCurrencySubtitle(rate)
+        bindCurrencyTitle(rate.code)
+        bindCurrencySubtitle(rate.code)
         bindAmountInput(rate, currencyOrAmountChangedFunc)
         bindCurrencyIcon(rate.code)
     }
 
-    private fun bindCurrencyTitle(rate: Rate) {
-        itemView.currencyCodeTextView!!.text = rate.code
+    private fun bindCurrencyTitle(currencyCode: String) {
+        itemView.currencyCodeTextView!!.text = currencyCode
     }
 
-    private fun bindCurrencySubtitle(rate: Rate) {
-        val currencyId = "currency_name_${rate.code.toLowerCase()}"
-        val stringResId = context.resources.getIdentifier(
-                currencyId, STRING_TYPE, BuildConfig.APPLICATION_ID)
-        itemView.currencyNameTextView.setText(
-                if (stringResId != 0) stringResId else R.string.currency_name_unknown)
+    private fun bindCurrencySubtitle(currencyCode: String) {
+        val stringResId = UiUtils.getStringResId(
+                context,
+                NAME_PREFIX,
+                currencyCode.toLowerCase(),
+                DEFAULT_NAME
+        )
+        itemView.currencyNameTextView.setText(stringResId)
     }
 
     private fun bindAmountInput(rate: Rate, currencyOrAmountChangedFunc: (String, Float) -> Unit) {
@@ -58,10 +64,12 @@ class CurrencyRateViewHolder(private val context: Context,
     }
 
     private fun bindCurrencyIcon(currencyCode: String) {
-        val currencyId = "cur_icon_${currencyCode.toLowerCase()}"
-        val iconResId = context.resources.getIdentifier(
-                currencyId, DRAWABLE_TYPE, BuildConfig.APPLICATION_ID)
-        itemView.currencyIconImageView.setImageResource(
-                if (iconResId != 0) iconResId else R.drawable.cur_icon_unknown)
+        val iconResId = UiUtils.getDrawableResId(
+                context,
+                ICON_PREFIX,
+                currencyCode.toLowerCase(),
+                DEFAULT_ICON
+        )
+        Picasso.with(context).load(iconResId).into(itemView.currencyIconImageView)
     }
 }
