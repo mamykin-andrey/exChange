4ec10d9ecaebc7b5bb7e8e6758b3ff6cb314e417
Mamykin Andrey <mamykin.av@bk.ru>
2018-08-29 22:59:10 +0500

    Add RatesLocalDataSource test

---
+++ app/src/test/java/ru/mamykin/exchange/data/repository/datasource/local/RatesLocalDataSourceTest.kt
@@ -0,0 +1,38 @@
+package ru.mamykin.exchange.data.repository.datasource.local
+
+import org.junit.Before
+import org.junit.Test
+import ru.mamykin.exchange.data.repository.datasource.RatesDataSource
+import ru.mamykin.exchange.domain.entity.RateList
+import java.util.*
+
+class RatesLocalDataSourceTest {
+
+    companion object {
+        private const val RUB_CURRENCY = "RUB"
+    }
+
+    lateinit var dataSource: RatesDataSource
+
+    @Before
+    fun setUp() {
+        dataSource = RatesLocalDataSource()
+    }
+
+    @Test
+    fun getRates_shouldReturnEmptyItem_inFirstTime() {
+        val testObserver = dataSource.getRates(RUB_CURRENCY).test()
+
+        testObserver.assertNoErrors().assertNoValues()
+    }
+
+    @Test
+    fun cacheRates_shouldSetLastRatesToNewValue() {
+        val rateList = RateList(RUB_CURRENCY, Date(), listOf())
+        dataSource.cacheRates(rateList)
+
+        val testObserver = dataSource.getRates(RUB_CURRENCY).test()
+
+        testObserver.assertNoErrors().assertValue(rateList)
+    }
+}
